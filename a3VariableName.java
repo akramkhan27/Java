0d1fdf3cf205dbf6c3b7d39d446589df94b32af9
akramkhan_27 <akramkhan.cse37@gmail.com>
2024-10-15 00:31:20 +0530

    Variable name

---
+++ a3VariableName.java
@@ -0,0 +1,31 @@
+public class a3VariableName {
+    public static void main(String[] args){
+        // variable name is case sensitive
+        // can be letters, digits, dollarSign $, underScore _
+        // Must not begin with digits 
+        // cannot use java keywords as variable names
+        // use camelCase while creating variables
+        // create meaningfull variable name
+
+        int amount=20;
+        int Amount=25;
+
+        int Value=1000;
+        int Value$=2000;
+        int Value_=3000;
+        int Value23=3000;
+
+        // int 2temp=215 // error
+        int _temp=200;
+        int $temp=300;
+        int temp=400;
+
+        String myFullName="Akram Khan";
+
+        String x="Akram";
+        String name="Akram";
+
+
+
+    }
+}
