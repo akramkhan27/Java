1d260197462f8367d28c4e37d318f7052329f802
akramkhan_27 <akramkhan.cse37@gmail.com>
2024-11-13 00:09:05 +0530

    Math class

---
+++ a60MathClass.java
@@ -0,0 +1,14 @@
+public class a60MathClass {
+    public static void main(String[] args) {
+        System.out.println(Math.max(10, 20));
+        System.out.println(Math.min(10, 20));
+        System.out.println(Math.PI);
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.round(1.2));
+        System.out.println(Math.ceil(2.3));
+        System.out.println(Math.floor(2.9));
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.pow(2, 3));
+        System.out.println((int)Math.floor(Math.random()*10+1));
+    }
+}
